b15b5a5f13e1559cb0799e36d4ce48751066ebaf
it63070112 <73678816+it63070112@users.noreply.github.com>
2020-11-19 20:29:26 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,7 +1,7 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'pimpimchanok119.mysql.database.azure.com', 'Pimchanok119@pimpimchanok119', 'PPNuro26', 'ITFLab', 3306);
+mysqli_real_connect($conn, 'labnew13.mysql.database.azure.com', 'pps@labnew13', 'Pornpaisan12', 'ITFLab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL PLEASE TRY AGAIN: '.mysqli_connect_error());
