79afbcdfc22f76d89de9111df75e01e678fa9196
it63070112 <73678816+it63070112@users.noreply.github.com>
2020-11-19 21:02:29 +0700

    Update show.php

--- show.php
+++ show.php
@@ -23,11 +23,11 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
     <div class="form">
         <p class="ex1"><h1><center>;-;FORM;-;</center></h1></p>
     </div>
-  <table width="600" border="1" class="table table-dark table-striped" class="center" class="ex1">
+  <table width="750" border="1" class="table table-dark table-striped" class="center" class="ex1">
     <tr class="info">
       <th width="200"> <div align="center">Name</div></th>
       <th width="350"> <div align="center">Comment </div></th>
-      <th width="50"> <div align="center">Action</div></th>
+      <th width="150"> <div align="center">Action</div></th>
     </tr>
   <?php
   while($Result = mysqli_fetch_array($res))
