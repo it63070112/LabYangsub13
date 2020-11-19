525babc922e3bd0798c17e540c7daefc3fbb4cb9
it63070112 <73678816+it63070112@users.noreply.github.com>
2020-11-19 20:42:36 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -16,7 +16,7 @@ $sql = "UPDATE guestbook SET Comment='$comment' WHERE Name='$name' ";
 
 
 if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully! CONGRATULATION!";
+    echo "New record created successfully";
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
